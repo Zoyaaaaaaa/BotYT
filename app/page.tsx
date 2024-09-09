bc3463d1b517118f8463e2b5fd2ad2f9606b0ea4
Zoyaaaaaaa <zoyah768@gmail.com>
2024-09-09 21:19:03 +0530

    Deploying on vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import { SummaryForm } from "@/components/Form";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import LoginPage from "./login/page";
+
 import YoutubeBot from "@/components/SummaryForm";
-// import { Sum } from "@/components/Sum";
+
 
 export default function Home() {
   return (
-
     <>
  <YoutubeBot/>
   
